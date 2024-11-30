682020be39da9c5b7e70504a83baa2ad1f7d9113
IvannEmmanuel <F0dG1ngD13@gmail.com>
2024-12-01 03:59:21 +0800

    added reactions and edit delete tapos chatpage

--- components/Chats.js
+++ components/Chats.js
@@ -9,14 +9,19 @@ import {
   Keyboard,
   ActivityIndicator,
   Dimensions,
+  Animated,
 } from "react-native";
 import { auth, db } from "../firebaseConfig";
 import {
   collection,
   query,
+  setDoc,
+  deleteDoc,
+  doc,
   onSnapshot,
   addDoc,
   orderBy,
+  deleteField,
 } from "firebase/firestore";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { format } from "date-fns";
@@ -27,12 +32,39 @@ const { height, width } = Dimensions.get("window");
 const Chats = () => {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
-  const [loading, setLoading] = useState(true); // Loading state
+  const [loading, setLoading] = useState(true);
+  const [friendIsTyping, setFriendIsTyping] = useState(false);
+  const [messageOptionsVisible, setMessageOptionsVisible] = useState(false);
+  const [selectedMessage, setSelectedMessage] = useState(null);
   const route = useRoute();
-  const navigation = useNavigation(); // Access navigation
+  const navigation = useNavigation();
   const { friend } = route.params;
   const inputRef = useRef(null);
   const flatListRef = useRef(null);
+  const bounceAnim = useRef(new Animated.Value(0)).current;
+  const [editingMessageId, setEditingMessageId] = useState(null);
+  const [shownReactionsMessageId, setShownReactionsMessageId] = useState(null);
+
+  const reactions = ["❤️", "😢", "😮", "😠"];
+
+  useEffect(() => {
+    if (friendIsTyping) {
+      Animated.loop(
+        Animated.sequence([
+          Animated.timing(bounceAnim, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true,
+          }),
+          Animated.timing(bounceAnim, {
+            toValue: 0,
+            duration: 500,
+            useNativeDriver: true,
+          }),
+        ])
+      ).start();
+    }
+  }, [friendIsTyping]);
 
   useEffect(() => {
     const currentUser = auth.currentUser;
@@ -49,6 +81,7 @@ const Chats = () => {
           id: doc.id,
           ...data,
           createdAt: format(data.createdAt.toDate(), "MMM dd, yyyy hh:mm a"),
+          reactions: data.reactions || {},
         };
         messagesList.push(formattedData);
       });
@@ -63,42 +96,159 @@ const Chats = () => {
       );
 
       setMessages(filteredMessages);
-      setLoading(false); // Loading complete
+      setLoading(false);
     });
 
-    return () => unsubscribe();
-  }, [friend.id]);
+    const typingRef = doc(db, "typingStatus", friend.email);
+    const unsubscribeTyping = onSnapshot(typingRef, (docSnapshot) => {
+      if (docSnapshot.exists()) {
+        setFriendIsTyping(docSnapshot.data().isTyping);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+      unsubscribeTyping();
+    };
+  }, [friend.email]);
+
+  const handleLongPress = (messageId, messageText) => {
+    if (selectedMessage?.id === messageId) {
+      // If the same message is long-pressed again, hide options
+      setSelectedMessage(null);
+      setMessageOptionsVisible(false);
+      setShownReactionsMessageId(null);
+    } else {
+      setSelectedMessage({ id: messageId, text: messageText });
+      setMessageOptionsVisible(true);
+      setShownReactionsMessageId(messageId);
+    }
+  };
+
+  const handleReaction = async (reaction, messageId) => {
+    const currentUser = auth.currentUser;
+    const messageRef = doc(db, "messages", messageId);
+
+    const message = messages.find((m) => m.id === messageId);
+    const currentUserReaction = message.reactions[currentUser.email];
+
+    await setDoc(
+      messageRef,
+      {
+        reactions: {
+          ...message.reactions,
+          [currentUser.email]:
+            currentUserReaction === reaction ? deleteField() : reaction,
+        },
+      },
+      { merge: true }
+    );
+
+    setShownReactionsMessageId(null);
+  };
+
+  const handleEditMessage = async (newMessageText) => {
+    const messageRef = doc(db, "messages", selectedMessage.id);
+    await setDoc(messageRef, { text: newMessageText }, { merge: true });
+    setMessageOptionsVisible(false);
+    setSelectedMessage(null);
+  };
+
+  const handleDeleteMessage = async () => {
+    const messageRef = doc(db, "messages", selectedMessage.id);
+    await deleteDoc(messageRef);
+    setMessages((prevMessages) =>
+      prevMessages.filter((message) => message.id !== selectedMessage.id)
+    );
+    setMessageOptionsVisible(false);
+    setSelectedMessage(null);
+  };
 
   const handleSendMessage = async () => {
     if (newMessage.trim()) {
-      const currentUser = auth.currentUser;
-      await addDoc(collection(db, "messages"), {
-        text: newMessage,
-        senderId: currentUser.email,
-        receiverId: friend.email,
-        createdAt: new Date(),
-      });
       setNewMessage("");
       Keyboard.dismiss();
-      if (inputRef.current) {
-        inputRef.current.clear();
+
+      const currentUser = auth.currentUser;
+
+      if (editingMessageId) {
+        await handleEditMessage(newMessage);
+        setEditingMessageId(null);
+      } else {
+        await addDoc(collection(db, "messages"), {
+          text: newMessage,
+          senderId: currentUser.email,
+          receiverId: friend.email,
+          createdAt: new Date(),
+          reactions: {},
+        });
       }
     }
   };
 
-  const renderMessageItem = ({ item }) => (
-    <View
-      style={[
-        styles.message,
-        item.senderId === auth.currentUser.email
-          ? styles.sentMessage
-          : styles.receivedMessage,
-      ]}
-    >
-      <Text style={styles.messageText}>{item.text}</Text>
-      <Text style={styles.timestamp}>{item.createdAt}</Text>
-    </View>
-  );
+  const handleTyping = (text) => {
+    setNewMessage(text);
+
+    const currentUser = auth.currentUser;
+    const typingRef = doc(db, "typingStatus", currentUser.email);
+
+    setDoc(typingRef, { isTyping: text.trim().length > 0 });
+  };
+
+  const renderMessageReactions = (item) => {
+    const reactionCounts = {};
+    Object.values(item.reactions || {}).forEach((reaction) => {
+      reactionCounts[reaction] = (reactionCounts[reaction] || 0) + 1;
+    });
+
+    return (
+      <View style={styles.reactionContainer}>
+        {Object.entries(reactionCounts).map(([reaction, count]) => (
+          <Text key={reaction} style={styles.reactionText}>
+            {reaction} {count > 1 ? count : ""}
+          </Text>
+        ))}
+      </View>
+    );
+  };
+
+  const renderMessageItem = ({ item }) => {
+    const isOwnMessage = item.senderId === auth.currentUser.email;
+
+    return (
+      <TouchableOpacity
+        style={[
+          styles.message,
+          isOwnMessage ? styles.sentMessage : styles.receivedMessage,
+        ]}
+        onLongPress={() => handleLongPress(item.id, item.text)}
+      >
+        <Text style={styles.messageText}>{item.text}</Text>
+        <Text style={styles.timestamp}>{item.createdAt}</Text>
+
+        {renderMessageReactions(item)}
+
+        {shownReactionsMessageId === item.id && (
+          <View
+            style={[
+              styles.reactionsPopup,
+              isOwnMessage ? { right: 0 } : { left: 0 },
+              { zIndex: 999 },
+            ]}
+          >
+            {reactions.map((reaction) => (
+              <TouchableOpacity
+                key={reaction}
+                onPress={() => handleReaction(reaction, item.id)}
+              >
+                <Text style={styles.reactionEmoji}>{reaction}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        )}
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <View style={styles.container}>
@@ -131,34 +281,61 @@ const Chats = () => {
               flatListRef.current.scrollToEnd({ animated: true });
             }
           }}
+          onScrollBeginDrag={() => setShownReactionsMessageId(null)}
         />
+      )}
+
+      {friendIsTyping && (
+        <Animated.View
+          style={[
+            styles.typingIndicator,
+            {
+              opacity: bounceAnim.interpolate({
+                inputRange: [0, 0.5, 1],
+                outputRange: [1, 0.5, 1],
+              }),
+            },
+          ]}
+        >
+          <Text>{friend.username} is typing...</Text>
+        </Animated.View>
+      )}
+
+      {messageOptionsVisible && selectedMessage && (
+        <View style={styles.messageOptions}>
+          <TouchableOpacity
+            style={styles.option}
+            onPress={() => handleEditMessage(newMessage)}
+          >
+            <Text>Edit</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.option} onPress={handleDeleteMessage}>
+            <Text>Delete</Text>
+          </TouchableOpacity>
+        </View>
       )}
 
       <View style={styles.inputContainer}>
         <TextInput
           ref={inputRef}
-          style={styles.input}
-          placeholder="Type a message..."
           value={newMessage}
-          onChangeText={setNewMessage}
-          onSubmitEditing={handleSendMessage}
-          returnKeyType="send"
+          onChangeText={handleTyping}
+          placeholder="Type a message"
+          style={styles.input}
         />
-        <TouchableOpacity style={styles.sendButton} onPress={handleSendMessage}>
-          <Text style={styles.sendButtonText}>Send</Text>
+        <TouchableOpacity onPress={handleSendMessage} style={styles.sendButton}>
+          <Ionicons name="send" size={24} color="white" />
         </TouchableOpacity>
       </View>
     </View>
   );
 };
 
-export default Chats;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f9f9f9",
-    paddingHorizontal: 16,
+    padding: 10,
+    backgroundColor: "#fff",
   },
   headerContainer: {
     flexDirection: "row",
@@ -173,68 +350,108 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 2,
     marginBottom: 16,
-    marginTop: height * 0.05, // Adjust header to be a bit lower from the top
+    marginTop: height * 0.03,
   },
   header: {
     fontSize: 20,
-    fontWeight: "600",
-    color: "#333",
-    marginLeft: 15,
-    flex: 1,
+    fontWeight: "bold",
+    marginLeft: 8,
   },
   messageList: {
-    paddingBottom: 16,
+    paddingBottom: 50,
   },
   message: {
+    marginBottom: 10,
     padding: 10,
     borderRadius: 8,
-    marginVertical: 4,
     maxWidth: "80%",
+    position: "relative",
   },
   sentMessage: {
-    backgroundColor: "#e6f7ff",
+    backgroundColor: "#d1f0d1",
     alignSelf: "flex-end",
   },
   receivedMessage: {
-    backgroundColor: "#d1ffd1",
+    backgroundColor: "#f1f1f1",
     alignSelf: "flex-start",
   },
   messageText: {
     fontSize: 16,
-    color: "#333",
   },
   timestamp: {
     fontSize: 12,
-    color: "#888",
-    marginTop: 4,
+    color: "gray",
+    marginTop: 5,
+  },
+  reactionContainer: {
+    flexDirection: "row",
+    marginTop: 5,
+  },
+  reactionText: {
+    fontSize: 14,
+    marginRight: 5,
+  },
+  reactionsPopup: {
+    position: "absolute",
+    bottom: 50,
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  reactionEmoji: {
+    fontSize: 24,
+  },
+  typingIndicator: {
+    position: "absolute",
+    bottom: 20,
+    left: 10,
   },
   inputContainer: {
     flexDirection: "row",
     alignItems: "center",
-    padding: 10,
     borderTopWidth: 1,
-    borderColor: "#ccc",
+    borderTopColor: "#ccc",
+    paddingVertical: 10,
   },
   input: {
     flex: 1,
+    borderRadius: 20,
     borderWidth: 1,
     borderColor: "#ccc",
-    borderRadius: 20,
-    padding: 10,
-    marginRight: 10,
+    paddingLeft: 10,
+    paddingVertical: 5,
   },
   sendButton: {
+    marginLeft: 10,
     backgroundColor: "#007bff",
-    borderRadius: 20,
     padding: 10,
-  },
-  sendButtonText: {
-    color: "#fff",
-    fontWeight: "bold",
+    borderRadius: 50,
   },
   loadingIndicator: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
+  messageOptions: {
+    position: "absolute",
+    bottom: 10,
+    left: "50%",
+    transform: [{ translateX: -50 }],
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    zIndex: 999,
+  },
+  option: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+  },
 });
+
+export default Chats;
--- pages/screens/ChatPage.js
+++ pages/screens/ChatPage.js
@@ -6,10 +6,13 @@ import {
   FlatList,
   Image,
   TouchableOpacity,
+  Dimensions
 } from "react-native";
 import { auth, db } from "../../firebaseConfig";
 import { doc, onSnapshot } from "firebase/firestore";
 import { useNavigation } from "@react-navigation/native";
+
+const { height, width } = Dimensions.get("window");
 
 const ChatPage = () => {
   const [friends, setFriends] = useState([]);
@@ -60,22 +63,19 @@ const ChatPage = () => {
 
   const renderFriendItem = ({ item }) => (
     <TouchableOpacity style={styles.friendItem} onPress={() => handleChat({ ...item, id: item.id || item.email })}>
-      <Image source={{ uri: item.profilePictureURL }} style={styles.profilePicture} />
+      <View style={styles.header}>
+        <Image source={{ uri: item.profilePictureURL }} style={styles.profilePicture} />
+      </View>
       <View style={styles.friendDetails}>
         <Text style={styles.friendName}>{item.username}</Text>
         <Text style={styles.friendEmail}>{item.email}</Text>
-        {item.gender && (
-          <Text style={styles.friendGender}>
-            Gender: {item.gender}
-          </Text>
-        )}
       </View>
     </TouchableOpacity>
   );
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Friends</Text>
+      <Image source={require('../../images/TALKO.png')} style={styles.talkoPic} />
       {loading ? (
         <Text style={styles.loadingText}>Loading friends...</Text>
       ) : friends.length > 0 ? (
@@ -97,15 +97,13 @@ export default ChatPage;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f9f9f9",
+    backgroundColor: "#f2f2f2", // Lighter background for a cleaner look
     padding: 16,
   },
-  header: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#333",
-    alignSelf: "center",
-    marginBottom: 16,
+  talkoPic:{
+    height: height * 0.2,
+    width: height * 0.2,
+    alignSelf: 'center'
   },
   friendList: {
     paddingBottom: 16,
@@ -114,22 +112,29 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     padding: 15,
-    backgroundColor: "#e6f7ff",
-    borderRadius: 8,
+    backgroundColor: "#ffffff", // White background for friend items
+    borderRadius: 10, // Rounded corners for a smoother look
+    shadowColor: "#000", // Shadow for elevation effect
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3, // Adds elevation for Android
     marginVertical: 8,
   },
   profilePicture: {
     width: 50,
     height: 50,
     borderRadius: 25,
-    marginRight: 10,
+    marginRight: 15,
+    borderWidth: 1,
+    borderColor: "#d1d1d1", // Light border around profile picture
   },
   friendDetails: {
     flex: 1,
   },
   friendName: {
     fontSize: 18,
-    color: "#333",
+    color: "#333", // Dark text for readability
     fontWeight: "bold",
   },
   friendEmail: {
